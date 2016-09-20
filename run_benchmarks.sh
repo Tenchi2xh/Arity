eb3e573f64e2eead79eddfd9710db17821a3ecc2
Hamza Haiken <hamza.haiken@ordina.nl>
2016-09-20 10:50:44 +0200

    Fix benchmark script

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -4,4 +4,4 @@ PYTEST=pytest
 if ! type -P "$PYTEST" > /dev/null; then
   PYTEST=py.test
 fi
-PYTHONPATH=. pytest benchmarks -v # --benchmark-warmup-iterations 1 --benchmark-max-time 2
+PYTHONPATH=. $PYTEST benchmarks -v # --benchmark-warmup-iterations 1 --benchmark-max-time 2
